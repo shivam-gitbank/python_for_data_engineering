63408e1940260a1e16c709353f178af0a35025ef
ss5030200@gmail.com <ss5030200@gmail.com>
2026-02-09 19:44:47 +0000

    go

---
+++ API.py
@@ -0,0 +1,20 @@
+import requests # request lib
+import json # json lib
+import boto3
+from datetime import datetime, timezone
+
+#-----------------------------------------------------
+# Config
+#-----------------------------------------------------
+
+API_URL = ""
+S3_BUCKET = ""
+SOURCE_NAME = ""
+
+#----------------------------------------------------
+# Calling API
+#----------------------------------------------------
+response = requests.get(API_URL, timeout = 30)
+response.raise_for_status()
+
+api_data = response.json()
